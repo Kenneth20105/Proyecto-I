211515eb835b5cd4e35adc2981b8b077162d28e2
Kenneth20105 <kfjvasquez@gmail.com>
2025-05-08 18:03:01 -0600

    Update GestorBiblioteca.java

--- src/gui/GestorBiblioteca.java
+++ src/gui/GestorBiblioteca.java
@@ -7,16 +7,19 @@ import persistence.DocumentoDAO;
 import persistence.PrestamoDAO;
 import persistence.UsuarioDAO;
 
-import java.sql.Date;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.List;
 
 public class GestorBiblioteca {
     private final UsuarioDAO usuarioDAO = new UsuarioDAO();
-    private final DocumentoDAO documentoDAO = null;
+    private final DocumentoDAO documentoDAO;
 
     private final PrestamoDAO prestamoDAO = new PrestamoDAO();
+
+    public GestorBiblioteca() throws SQLException {
+        documentoDAO = new DocumentoDAO();
+    }
 
     public void agregarUsuario(String nombre, String tipo) throws SQLException {
         Usuario u = new Usuario(nombre, tipo);
@@ -51,5 +54,23 @@ public class GestorBiblioteca {
 
     public List<Prestamo> obtenerPrestamosActivos() throws SQLException {
         return prestamoDAO.obtenerPrestamosActivos();
+    }
+    public String validarCredenciales(String usuario, String contrasena) throws SQLException {
+        String sql = "SELECT rol FROM usuarios WHERE username = ? AND password = ?";
+
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca_donbosco", "root", "kenny3.01")) {
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+                stmt.setString(1, usuario);
+                stmt.setString(2, contrasena);
+
+                try (ResultSet rs = stmt.executeQuery()) {
+                    if (rs.next()) {
+                        return rs.getString("rol");
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
