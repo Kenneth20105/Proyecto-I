dd13fcdc797f589bfc4cd67e84ee668bdec5d499
Kenneth20105 <kfjvasquez@gmail.com>
2025-05-08 18:06:32 -0600

    Create Usuario.java

---
+++ src/model/Usuario.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String tipo;
+
+    public Usuario(int id, String nombre, String tipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public Usuario(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public int getId() { return id; }
+    public String getNombre() { return nombre; }
+    public String getTipo() { return tipo; }
+
+    public void setId(int id) { this.id = id; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + tipo + ")";
+    }
+}
